0ddf1f632bc54a1278659bfa3979bfd1c12c7aab
Emerson Dias <edias7942@gmail.com>
2023-12-25 15:06:37 +0000

    Criando o WeHaveCreation

---
+++ home/script.js
@@ -0,0 +1,26 @@
+// MENU MOBILE
+
+let menuMobileOption = false;
+let menuMobile = document.getElementById("menu-mobile");
+let bortaoFecharMenu = document.getElementById('bota-fechar-menu')
+
+function toggleMenuMobile() {
+    if (menuMobileOption) {
+        for (i=1;i<=3;i++){
+            let div = document.getElementById(`line-${i}`)
+            let hamburguer = document.getElementById('hamburguer')
+            hamburguer.classList.remove('close')
+            div.classList.remove('close')
+        }
+        menuMobile.style.width = '0'
+    } else {
+        for (i=1;i<=3;i++){
+            let div = document.getElementById(`line-${i}`)
+            let hamburguer = document.getElementById('hamburguer')
+            hamburguer.classList.add('close')
+            div.classList.add('close')
+        }
+        menuMobile.style.width = 'calc(100vw - 90px)'
+    }
+    menuMobileOption = !menuMobileOption;
+}

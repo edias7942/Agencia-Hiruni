74a4391353d632953d90ef6a019070aaa695c16a
Emerson Dias <edias7942@gmail.com>
2024-01-16 18:52:26 -0300

    Adicionando animações

--- home/script.js
+++ home/script.js
@@ -23,3 +23,43 @@ function toggleMenuMobile() {
     }
     menuMobileOption = !menuMobileOption;
 }
+
+// MUDANDO DE IMAGEM
+
+let carouselItems = {
+    2: {
+        allowChange: true,
+        currentItem: 0,
+        itensLength: 2,
+        lastDirection: 1,
+        locationItem: "#section2>.container>#right-side>#item-",
+        itemsData: [
+            document.querySelector(`#section2>.container>#right-side>#item-0`),
+            document.querySelector(`#section2>.container>#right-side>#item-1`),
+            document.querySelector(`#section2>.container>#right-side>#item-2`),
+        ],
+    },
+    5: {
+        currentItem: 0,
+        itensLength: 10,
+        lastDirection: 1,
+    },
+    6: {
+        currentItem: 0,
+        itensLength: 10,
+        lastDirection: 1,
+    },
+};
+
+console.log(carouselItems[2].itemsData)
+
+let entrandoClass, saindoClass, classRemove, botoes, currentImage, prevImage;
+
+function changeImage(section, direction) {
+    let itemSaindo = carouselItems[section].itemsData[carouselItems[section].currentItem]
+    itemSaindo.classList.add("saindo-fundo")
+    carouselItems[section].currentItem += 1
+    console.log(itemSaindo)
+}
+
+changeImage(2,1)

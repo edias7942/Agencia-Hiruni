6c4e905e7ed0f00d3ade12347fed58ab9382e079
Emerson Dias <edias7942@gmail.com>
2024-01-18 18:50:53 -0300

    adicionando animações na seção 2 para ambos os lados

--- home/script.js
+++ home/script.js
@@ -1,25 +1,29 @@
 // MENU MOBILE
 
+// setTimeout(() => {
+//     scrollTo(0,0)
+// }, 100);
+
 let menuMobileOption = false;
 let menuMobile = document.getElementById("menu-mobile");
 
 function toggleMenuMobile() {
     if (menuMobileOption) {
-        for (i=1;i<=3;i++){
-            let div = document.getElementById(`line-${i}`)
-            let hamburguer = document.getElementById('hamburguer')
-            hamburguer.classList.remove('close')
-            div.classList.remove('close')
+        for (i = 1; i <= 3; i++) {
+            let div = document.getElementById(`line-${i}`);
+            let hamburguer = document.getElementById("hamburguer");
+            hamburguer.classList.remove("close");
+            div.classList.remove("close");
         }
-        menuMobile.style.width = '0'
+        menuMobile.style.width = "0";
     } else {
-        for (i=1;i<=3;i++){
-            let div = document.getElementById(`line-${i}`)
-            let hamburguer = document.getElementById('hamburguer')
-            hamburguer.classList.add('close')
-            div.classList.add('close')
+        for (i = 1; i <= 3; i++) {
+            let div = document.getElementById(`line-${i}`);
+            let hamburguer = document.getElementById("hamburguer");
+            hamburguer.classList.add("close");
+            div.classList.add("close");
         }
-        menuMobile.style.width = 'calc(100vw - 90px)'
+        menuMobile.style.width = "calc(100vw - 90px)";
     }
     menuMobileOption = !menuMobileOption;
 }
@@ -29,7 +33,7 @@ function toggleMenuMobile() {
 let carouselItems = {
     2: {
         allowChange: true,
-        currentItem: 0,
+        currentItem: 1,
         itensLength: 2,
         lastDirection: 1,
         locationItem: "#section2>.container>#right-side>#item-",
@@ -37,6 +41,9 @@ let carouselItems = {
             document.querySelector(`#section2>.container>#right-side>#item-0`),
             document.querySelector(`#section2>.container>#right-side>#item-1`),
             document.querySelector(`#section2>.container>#right-side>#item-2`),
+            document.querySelector(`#section2>.container>#right-side>#item-3`),
+            document.querySelector(`#section2>.container>#right-side>#item-4`),
+            document.querySelector(`#section2>.container>#right-side>#item-5`),
         ],
     },
     5: {
@@ -51,15 +58,80 @@ let carouselItems = {
     },
 };
 
-console.log(carouselItems[2].itemsData)
-
-let entrandoClass, saindoClass, classRemove, botoes, currentImage, prevImage;
+let entrandoClass,
+    saindoClass,
+    classRemove,
+    botoes,
+    currentItem,
+    leftItem,
+    mainItem,
+    rightItem;
 
 function changeImage(section, direction) {
-    let itemSaindo = carouselItems[section].itemsData[carouselItems[section].currentItem]
-    itemSaindo.classList.add("saindo-fundo")
-    carouselItems[section].currentItem += 1
-    console.log(itemSaindo)
-}
+    let currentItem = carouselItems[section].currentItem;
+    let itemsList = carouselItems[section].itemsData;
+    let itemsQuantity = itemsList.length - 1;
+    let lastDirection = carouselItems[section].lastDirection;
 
-changeImage(2,1)
+    if (direction === 1) {
+        leftItem = itemsList[currentItem];
+        mainItem = itemsList[currentItem + 1];
+        rightItem = itemsList[currentItem + 2];
+    } else {
+        leftItem = itemsList[currentItem - 1];
+        mainItem = itemsList[currentItem];
+        rightItem = itemsList[currentItem + 1];
+    }
+
+    console.log(currentItem);
+
+    if (direction === 1) {
+        leftItem.classList.add("saindo-fundo");
+
+        setTimeout(() => {
+            mainItem.classList.add("entrando-direita");
+            rightItem.classList.add("quase-entrando");
+        }, 660);
+
+        setTimeout(() => {
+            leftItem.classList.remove("saindo-fundo");
+        }, 1320);
+
+        if (lastDirection === 1) {
+            leftItem.classList.remove("entrando-direita");
+
+            setTimeout(() => {
+                mainItem.classList.remove("quase-entrando");
+            }, 660);
+        } else {
+            
+            setTimeout(() => {
+                leftItem.classList.remove('entrando-fundo')
+                mainItem.classList.remove('quase-entrando-esquerda')
+            }, 660);
+        }
+    } else {
+        leftItem.classList.add("entrando-fundo");
+        mainItem.classList.add("quase-entrando-esquerda");
+
+        setTimeout(() => {
+            rightItem.classList.remove("quase-entrando");
+            rightItem.classList.add("saindo-direita");
+        }, 660);
+
+        setTimeout(() => {
+            rightItem.classList.remove("saindo-direita");
+        }, 1200);
+
+        if (lastDirection === 1) {
+            mainItem.classList.remove("entrando-direita");
+        } else {
+            mainItem.classList.remove('entrando-fundo')
+            rightItem.classList.remove('quase-entrando-esquerda')
+        }
+    }
+
+    console.log(lastDirection);
+    carouselItems[2].currentItem += direction;
+    carouselItems[2].lastDirection = direction;
+}

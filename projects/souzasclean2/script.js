645b4a3cac8c728fbce475070a5e521db48f85dd
Emerson Dias <edias7942@gmail.com>
2024-04-09 18:15:12 -0300

    atualizando versao 2.0 souzasclean

--- projects/souzasclean2/script.js
+++ projects/souzasclean2/script.js
@@ -26,3 +26,37 @@ function changeClass(elementId, classRemove, classAdd) {
     if (classRemove) element.classList.remove(classRemove);
     if (classAdd) element.classList.add(classAdd);
 }
+
+// CRIANDO ANIMAÇÕES COM O SCROLL
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            // entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Lidando com o resize element do Antes e Depois
+
+const box = document.querySelector("#antes-depois-resize-element");
+var x, w;
+
+function ts(e) {
+    x = e.touches[0].clientX;
+    w = box.clientWidth;
+    console.log("Clicando");
+}
+
+function tm(e) {
+    mx = e.touches[0].clientX;
+    cx = mx - x;
+    box.style.width = cx + w + "px";
+    console.log("arrastando");
+}

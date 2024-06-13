f1b66465b4e1c0c1d0b7ef32d9482f4850e2c2f4
Emerson Dias <edias7942@gmail.com>
2024-06-13 18:04:32 -0300

    Finalizando animações da página

--- projects/wehavepaper/kit-congresso-2024/script.js
+++ projects/wehavepaper/kit-congresso-2024/script.js
@@ -1,1 +1,22 @@
-console.log(16%4)
+// CRIANDO ANIMAÇÕES COM O SCROLL
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            // entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+function clicando(scrollNumber) {
+    let alvo = document.getElementById('alvo');
+    console.log(alvo.scrollLeft)
+    alvo.scrollLeft += scrollNumber;
+};
